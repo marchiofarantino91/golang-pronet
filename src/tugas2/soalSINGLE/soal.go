ba8eb2aae7ae2fefa7712bdfa82bef56041736cf
marchiofarantino91 <marchiofarantino91@gmail.com>
2019-01-28 20:40:29 +0700

    update

--- src/tugas2/soalSINGLE/soal.go
+++ src/tugas2/soalSINGLE/soal.go
@@ -1,34 +1,36 @@
 package main
 
-import ("fmt"
-		"net"
-		"bufio"
+import (
+	"bufio"
+	"fmt"
+	"net"
 )
-func check(err error, message string)  {
+
+func check(err error, message string) {
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("%s\n", message)
 }
 
-func main()  {
-	ln, err := net.Listen("tcp", ":8080")
-	check(err,"server is ready.")
-	for{
-		conn,err := ln.Accept()
+func main() {
+	ln, err := net.Listen("tcp", ":80")
+	check(err, "server is ready.")
+	for {
+		conn, err := ln.Accept()
 		check(err, "Accepted Connection.")
 
-		go func(){
+		go func() {
 			buf := bufio.NewReader(conn)
 
-			for{
-				name,err := buf.ReadString('\n')
+			for {
+				name, err := buf.ReadString('\n')
 
-				if err != nil{
+				if err != nil {
 					fmt.Printf("Client Disconect")
 					break
 				}
-				conn.Write([]byte("Hello," +name))
+				conn.Write([]byte("Hello," + name))
 			}
 		}()
 	}
